b0717ac725f42262a78b17fdcebb450b7ebe3057
NelloRossi <rossianiello97@gmail.com>
2023-06-07 15:25:37 +0200

    added table type to db

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = config('types.technologies');
+        foreach ($types as $type) {
+            $newType = new Type;
+            $newType->name = $type['name'];
+            $newType->slug = Str::slug($type['name'], '-');
+            $newType->image = $type['image'];
+            $newType->description = $type['description'];
+            $newType->save();
+        }
+    }
+}
